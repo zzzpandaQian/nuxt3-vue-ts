d608691bdac5d59d41444f260583b483d0343145
joey <joey@anttoweb.com>
2022-06-13 17:00:18 +0800

    element-plus+vite+ts模板

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+// createWebHistory: history模式; createWebHashHistory: hash模式; createMemoryHistory ssr模式
+import {createRouter, createWebHistory, createMemoryHistory, createWebHashHistory} from 'vue-router'
+import home from '../views/home/index.vue'
+import mine from '../views/mine/index.vue'
+
+const routes = [
+    {
+        path: '/home',
+        component: home,
+        name: 'home'
+    },
+    {
+        path: '/mine',
+        component: mine,
+        name: 'mine'
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/mine/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {useRoute} from 'vue-router'
+
+const route = useRoute()
+console.log(route.query)
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
